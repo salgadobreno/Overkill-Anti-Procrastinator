35b3bd40ae0afeafd6d88d200e77439d662e9a19
Breno Salgado <salgado.breno@gmail.com>
2011-11-02 18:03:37 -0200

    Initial + 1 commit...anti_procrastinator is the file to be added to init.d, check source for customizing to yourself

--- script.rb
+++ script.rb
@@ -1,6 +1,6 @@
+#!/usr/bin/ruby
 require 'rubygems'
 require 'tempfile'
-require 'date'
 require 'fileutils'
 require 'observer'
 require 'ruby-debug'
@@ -13,28 +13,24 @@ class TimeNotifier
   #"17:00:00..19:00:00"
 
   CLOSED = [["09:30", "11:30"],["12","14"], ["14:30","16:30"], ["17:00","19:00"]]
-  BLOCKS = ["twitter.com", "facebook.com", "9gag.com", "g1.com.br", "globoesporte.com"]
+  BLOCKS = ["twitter.com", "facebook.com", "www.facebook.com","9gag.com", "g1.com.br", "globoesporte.com", "globoesporte.globo.com", "g1.globo.com"]
 
   def update(actual_time)
-    p "update"
     ranges = get_ranges(CLOSED)
     success = false
     ranges.each do |r|
-      if r === actual_time
+      if r.include? actual_time
         disable_procrastinators() unless @procrastination_disabled
         @procrastination_disabled = true
         @procrastination_enabled = nil
         success = true
-        break
-      else
-        enable_procrastinators() unless @procrastination_enabled
-        @procrastination_enabled = true
-        @procrastination_disabled = nil
-        success = true
       end
       break if success
-      p @procrastination_enabled
-      p @procrastination_disabled
+    end
+    unless success
+      enable_procrastinators() unless @procrastination_enabled
+      @procrastination_enabled = true
+      @procrastination_disabled = nil
     end
   end
 
@@ -56,7 +52,8 @@ class TimeNotifier
     temp.puts lines
     temp.close
 
-    FileUtils.mv temp.path, "failou.txt"
+    FileUtils.mv temp.path, "/etc/hosts"
+    FileUtils.chmod(0644, "/etc/hosts")
   end
 
   def enable_procrastinators
@@ -72,37 +69,35 @@ class TimeNotifier
     temp.puts lines
     temp.close
 
-    FileUtils.mv temp.path, "failou.txt"
+    FileUtils.mv temp.path, "/etc/hosts"
+    FileUtils.chmod(0644, "/etc/hosts")
   end
 
   def get_ranges(ary)
     now = Time.now
     ranges = ary.map do |arr|
       arrr = arr.map do |t|
-        DateTime.civil(now.year, now.month, now.day, t[0..1].to_i, t[3..4].to_i)
+        Time.local(now.year, now.month, now.day, t[0..1].to_i, t[3..4].to_i)
       end
       arrr[0]..arrr[1]
     end
   end
 end
 
-class ActualTime < DateTime
-  class << ActualTime
+class TimeFetcher < Time
+  class << TimeFetcher
     include Observable
   end
     add_observer TimeNotifier.new
 
   def self.fetch_time
-    time = DateTime.now
+    time = now
     changed
     notify_observers time
   end
 end
 
-while true
-  p "sleep"
-  ActualTime.fetch_time()
-  sleep 5
+loop do
+  TimeFetcher.fetch_time()
+  sleep 60
 end
-
-
